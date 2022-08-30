25e2878ca82b984b040047e0d597607535af8505
AnwarXahid <mdahzahid@gmail.com>
2022-08-30 09:26:49 -0700

    calculating portfolio value for every token...

--- portfolio-balance.js
+++ portfolio-balance.js
@@ -38,7 +38,6 @@ const getLatestPortfolioForParticularToken = (df, token) => {
 }
 
 
-
 // Method to get the portfolio value for a given date : format (yyyy-mm-dd)
 const getPortfolioValueForParticularDate = (df, date) => {
     let balance = 0;
@@ -62,6 +61,50 @@ const getPortfolioValueForParticularDate = (df, date) => {
 }
 
 
+// Method to get the portfolio value for a given date : format (yyyy-mm-dd)
+const getPortfolioForParticularTokenInParticularDate = (df, date, token) => {
+    let balance = 0;
+    let initialEpochAtGMT = new Date(date + 'T00:00:00.000+00:00').getTime() / 1000;
+    let finalEpochAtGMT = initialEpochAtGMT + 86400000;
+
+    function predicateTheTimestamp(timestamp) {
+        let intTimestamp = parseInt(timestamp);
+        return intTimestamp > initialEpochAtGMT && intTimestamp < finalEpochAtGMT;
+    }
+
+    df.filter(row => row.get(transHistCol_TOKEN) === token)
+        .filter(row => predicateTheTimestamp(row.get(transHistCol_TIMESTAMP)))
+        .cast(transHistCol_AMOUNT, Number)
+        .map(row => {
+            balance = row.get(transHistCol_TRANSACTION_TYPE) === "DEPOSIT" ?
+                balance + row.get(transHistCol_AMOUNT) :
+                balance - row.get(transHistCol_AMOUNT);
+        });
+
+    console.log("Portfolio Value for the token " + token + " in the " + date + " is : " + balance);
+}
+
+
+// Method to get the latest portfolio value for a given token input
+const getLatestPortfolioForEveryToken = df => {
+    let tokenBalanceMap = new Map();
+
+
+    // let balance = 0;
+    // df.filter(row => row.get(transHistCol_TOKEN) === token)
+    //     .cast(transHistCol_AMOUNT, Number)
+    //     .map(row => {
+    //         balance = row.get(transHistCol_TRANSACTION_TYPE) === "DEPOSIT" ?
+    //             balance + row.get(transHistCol_AMOUNT) :
+    //             balance - row.get(transHistCol_AMOUNT);
+    //     });
+    //
+    // console.log("Latest Portfolio Value for " + token + " is : " + balance);
+}
+
+
+
+
 
 // Read csv file and process data using data frame
 console.log("csv file loading started...\n");
@@ -81,10 +124,14 @@ fs.createReadStream(FILE_PATH)
                 let transactionHistoryDF = new DataFrame(transHistRows, transHistCol);
 
                 // using predefined methods based on options (input argument)
-                if (options.t) {
+                if (options.d && options.t) {
+                    getPortfolioForParticularTokenInParticularDate(transactionHistoryDF, options.d, options.t);
+                } else if (options.t) {
                     getLatestPortfolioForParticularToken(transactionHistoryDF, options.t);
                 } else if (options.d) {
                     getPortfolioValueForParticularDate(transactionHistoryDF, options.d);
+                } else {
+                    getLatestPortfolioForEveryToken(transactionHistoryDF);
                 }
 
             })
